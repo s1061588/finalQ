1c846f544f3987dc5d1e3144dfe37f75ff93670f
翁聖閔 <brucew9878989@gmail.com>
2021-06-24 19:01:33 +0800

    first

---
+++ final_410615885/final_410615885/Controllers/ExamController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace final_410615885.Controllers
+{
+    public class ExamController : Controller
+    {
+        // GET: Exam
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
